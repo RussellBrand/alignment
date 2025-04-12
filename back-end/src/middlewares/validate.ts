047d779ca71ebdbb84f77220571621b0d297e5c0
Russell Brand <brand@responsible.com>
2025-04-12 12:17:32 -0700

    Allow put to have id's in the payload

--- back-end/src/middlewares/validate.ts
+++ back-end/src/middlewares/validate.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { ZodSchema, ZodError } from "zod";
+import { ZodSchema, ZodError, z } from "zod";
 
 const validate =
   (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
@@ -12,4 +12,31 @@ const validate =
     }
   };
 
+const validatePutRequest =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Clone the body without the _id field to validate against the schema
+      const { _id, ...bodyWithoutId } = req.body;
+
+      schema.parse(bodyWithoutId);
+
+      // If _id exists in request body, ensure it matches URL param id
+      if (_id && req.params.id && _id !== req.params.id) {
+        return res.status(400).json({
+          error: [
+            {
+              message: "Body _id must match URL parameter id",
+              path: ["_id"],
+            },
+          ],
+        });
+      }
+      next();
+    } catch (error: unknown) {
+      const err = error as ZodError;
+      res.status(400).json({ error: err.errors });
+    }
+  };
+
 export default validate;
+export { validatePutRequest };
--- back-end/src/routes/crudRoutes.ts
+++ back-end/src/routes/crudRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import controller from "../controllers/crudController";
-import validate from "../middlewares/validate";
+import validate, { validatePutRequest } from "../middlewares/validate";
 import { ZodSchema } from "zod";
 import { Model } from "mongoose";
 
@@ -13,7 +13,7 @@ export default function createRoutes(
   router.post("/", validate(schema), controller.create(Model));
   router.get("/", controller.readAll(Model));
   router.get("/:id", controller.readOne(Model));
-  router.put("/:id", validate(schema), controller.update(Model));
+  router.put("/:id", validatePutRequest(schema), controller.update(Model));
   router.delete("/:id", controller.delete(Model));
 
   return router;
